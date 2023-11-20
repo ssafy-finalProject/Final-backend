90855df1f2c72eb86fa9f9e51aefae35d08e2f13
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2023-11-20 10:38:26 +0900

    delete fileRepository

--- final-back/src/main/java/com/ssafy/file/repository/FileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.file.repository;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import com.ssafy.file.FileDto;
-
-public interface FileRepository {
-	void fileUpload(FileDto filedto);
-}
