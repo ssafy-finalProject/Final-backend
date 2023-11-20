404f9b788b8c0bee2b92c8681804394acc584c74
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2023-11-20 10:42:02 +0900

    Delete final-back/src/main/java/com/ssafy/file/service/fileService.java

--- final-back/src/main/java/com/ssafy/file/service/fileService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.file.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import com.ssafy.file.FileDto;
-
-public interface FileService {
-	void fileUpload(FileDto filedto);
-}
